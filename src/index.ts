b35a6b8d0bf1f427cbb9b9f44f6c68ea61dcc666
stonega <xijieyin@gmail.com>
2024-11-14 14:37:26 +0800

    fix: price calculation

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import { betaTestnet } from "./contract/network";
 
 function getPriceInscrease(): number {
   const random = Math.random();
+  if (random < 0.1) {
+    return Math.floor(Math.random()) - 2;
+  }
   if (random < 0.8) {
     return Math.floor(Math.random()) + 1;
   }
   else {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 5) + 1;
   }
 }
 
