54851996de5ef2ef3dd1cf9bc9e9a2703b82edee
stonega <xijieyin@gmail.com>
2024-12-01 15:13:37 +0800

    fix: update price

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,8 +8,8 @@ export async function getTargetPrice(): Promise<number> {
   const endBtcPrice = prices.find((a: any) => a.time === hour)?.USD;
   const startPrice = 5;
   // const noise = Math.random() * 0.2 + 1;
-  const targetPrice = startPrice * (1 + (endBtcPrice - startBtcPrice) / startBtcPrice * 3);
-  return Number(targetPrice.toFixed(2));
+  const targetPrice = startPrice * (1 + (endBtcPrice - startBtcPrice) / startBtcPrice * 4);
+  return Number(targetPrice.toFixed(4));
 }
 
 // Get sell and buy price from order-books
