aa72e74a5edfda14922e9171e56e0f893fd08b80
stonega <xijieyin@gmail.com>
2024-12-05 18:31:15 +0800

    fix: minor fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { getPrice, getTargetPrice } from "./utils";
 function getPriceInscrease(): number {
   const random = Math.random();
   if (random < 0.9) {
-    return Math.max(Math.random() * 0.02, 0.01);
+    return Math.max(Math.random() * 0.05, 0.01);
   }
   return -Math.max(Math.random() * 0.02, 0.01);
 }
