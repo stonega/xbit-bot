4e890e6d4fbe59f09b36c316c5a28035be8bf66b
stonega <xijieyin@gmail.com>
2024-12-06 18:45:02 +0800

    fix: update config

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ async function main(): Promise<void> {
     const nextSellPrice = Math.abs(Number(buyPrice) + getPriceInscrease());
 
     const price = nextSellPrice.toString();
-    const amount = 0.5 * Math.random() + 0.3;
+    const amount = 0.5 * Math.random() + 0.5;
     const receive = trade.calcUsdt(price, amount.toString());
     const sellRes = await trade.createSellOrder(signer, {
       amount: BigInt(parseEther(amount.toString())),
@@ -93,7 +93,7 @@ async function main(): Promise<void> {
       // Sell bool
       const nextSellPrice = Math.abs(Number(buyPrice) - getPriceInscrease());
       const price = nextSellPrice.toString();
-      const amount = 0.8 + Math.random();
+      const amount = 0.6 + Math.random();
       const receive = trade.calcUsdt(price, amount.toString());
       const sellRes = await trade.createSellOrder(signer, {
         amount: BigInt(parseEther(amount.toString())),
