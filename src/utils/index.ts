ec0a11191d86685bc38819df42fb1d809365ee0e
stonega <xijieyin@gmail.com>
2024-12-08 13:48:10 +0800

    fix: minor fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,21 @@
 const offsetMap: Map<string, number> = new Map();
 export async function getTargetPrice(): Promise<number> {
+  // 4 hour as as update intervel
   const currentHour = Math.floor(new Date().getTime() / 1000 / 60 / 60 / 4).toString();
+  // Get saved offet from memery
   let offset = offsetMap.get(currentHour);
-  const reset = Math.random() < 0.02;
+  const reset = Math.random() < 0.01;
   if (!offset || reset) {
     offsetMap.set(currentHour, Math.floor(Math.random() * 1000) + 250);
     offset = offsetMap.get(currentHour);
   }
   const priceResult = await fetch(`https://mempool.space/api/v1/historical-price`).then(a => a.json());
   const prices = priceResult.prices;
+  // From a static time
   const startHour = 1732582800 - 3600 * offset!;
   const startBtcPrice = prices.find((a: any) => a.time === startHour)?.USD;
   const startPrice = 3;
-  // const noise = Math.random() * 0.2 + 1;
-  const targetPrice = startPrice * (1 + (startBtcPrice - 50000) / 50000);
+  const targetPrice = startPrice * (1 + (startBtcPrice - 40000) / 40000);
   return Number(targetPrice.toFixed(4));
 }
 
