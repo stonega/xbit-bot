a2f3bbce3b456fa4dc73ba9843e1672adb6925b2
stonega <xijieyin@gmail.com>
2024-12-18 16:52:21 +0800

    fix: update config

--- src/index.ts
+++ src/index.ts
@@ -152,7 +152,7 @@ const task = new Task(
 );
 
 const role = Bun.env.ROLE || "maker";
-const duration = role === "MAKER" ? 10 : 15;
+const duration = role === "MAKER" ? 7 : 10;
 const job = new SimpleIntervalJob({ seconds: duration, runImmediately: true }, task);
 
 scheduler.addSimpleIntervalJob(job);
