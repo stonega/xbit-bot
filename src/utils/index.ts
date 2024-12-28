94d4b6b83751a9248b60c5cc736a9ef24131f061
stonega <xijieyin@gmail.com>
2024-12-28 17:52:08 +0800

    fix: Adjust price change threshold
    Increase price change threshold to 0.002
    to avoid excessive updates.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -71,12 +71,12 @@ export async function getTargetPrice(): Promise<number> {
   const headers = getHeaders(timestamp, "GET", requestPath, queryString);
   const data = await fetch(`${baseUrl}${requestPath}?${queryString}`, { headers }).then(res => res.json());
   let price = Number(data.data[0][1]) / 50;
-  if (Math.abs(price - lastPrice) > 0.001) {
+  if (Math.abs(price - lastPrice) > 0.002) {
     lastPrice = price;
   }
   else {
     lastPrice = price;
-    price += (price > lastPrice ? 0.001 : -0.001);
+    price += (price > lastPrice ? 0.002 : -0.002);
   }
   return price;
 }
