2c07f9b6ca9f85bd87c034801846b94897d5d0d1
stonega <xijieyin@gmail.com>
2024-12-28 16:44:21 +0800

    fix: Adjust price threshold for taker

--- src/index.ts
+++ src/index.ts
@@ -89,7 +89,7 @@ async function main(): Promise<void> {
 
   if (role === "taker") {
     let priceIncrease = getPriceTakerInscrease();
-    if (Math.abs(Number(buyPrice) - target) < 0.001) {
+    if (Math.abs(Number(buyPrice) - target) < 0.0001) {
       priceIncrease = 0.00012;
       console.debug(`[${new Date().toISOString()}] Target price reached, price inscrease ${priceIncrease}`);
       // If target price reached, add small random price increase
