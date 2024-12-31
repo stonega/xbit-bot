7dbc0399f31d1a7fc9014bc3af70f05aa98360c8
stonega <xijieyin@gmail.com>
2024-12-31 16:26:46 +0800

    fix: Update job duration based on role

--- src/index.ts
+++ src/index.ts
@@ -173,7 +173,7 @@ const task = new Task(
 );
 
 const role = Bun.env.ROLE || "maker";
-const duration = role === "MAKER" ? 7 : 10;
+const duration = role === "MAKER" ? 15 : 20;
 const job = new SimpleIntervalJob({ seconds: duration, runImmediately: true }, task);
 
 scheduler.addSimpleIntervalJob(job);
