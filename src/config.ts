b29acfc5e88f21f985b114899dc5e65d9a2c4688
stonega <xijieyin@gmail.com>
2025-01-07 01:05:40 +0800

    feat: Add STK/USDC and STK/BTU trading pairs

--- src/config.ts
+++ src/config.ts
@@ -6,18 +6,18 @@ export const pairs = [
     taker: Bun.env.TBOL_TAKER_PRIVATE_KEY,
     maker: Bun.env.TBOL_MAKER_PRIVATE_KEY,
   },
-  // {
-  //   symbol: "STK/USDC",
-  //   price: "BNB",
-  //   trade: "0x4856e15c4c2372f4a43295eeada4020a395a4340",
-  //   taker: Bun.env.STK_TAKER_PRIVATE_KEY,
-  //   maker: Bun.env.STK_MAKER_PRIVATE_KEY,
-  // },
-  // {
-  //   symbol: "STK/BTU",
-  //   price: "TON",
-  //   trade: "0x6a72e6efe3e54dcd7fb8af4ae7c7ba00189ed064",
-  //   taker: Bun.env.BTU_TAKER_PRIVATE_KEY,
-  //   maker: Bun.env.BTU_MAKER_PRIVATE_KEY,
-  // },
+  {
+    symbol: "STK/USDC",
+    price: "BNB",
+    trade: "0x4856e15c4c2372f4a43295eeada4020a395a4340",
+    taker: Bun.env.STK_TAKER_PRIVATE_KEY,
+    maker: Bun.env.STK_MAKER_PRIVATE_KEY,
+  },
+  {
+    symbol: "STK/BTU",
+    price: "TON",
+    trade: "0x6a72e6efe3e54dcd7fb8af4ae7c7ba00189ed064",
+    taker: Bun.env.BTU_TAKER_PRIVATE_KEY,
+    maker: Bun.env.BTU_MAKER_PRIVATE_KEY,
+  },
 ];
