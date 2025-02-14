425a47124e193291c8e07520e52c1adb5a1ec1f4
stone <xijieyin@gmail.com>
2025-02-14 14:53:54 +0800

    fix: Adjust buy amount based on sell amount
    Limit the buy amount when sellAmount is small, preventing stuck trades.

--- src/index.ts
+++ src/index.ts
@@ -133,11 +133,11 @@ async function main(pair: { price: string; symbol: string; trade?: string; taker
       // Buy bool to increase price
       let amount = 4 + Math.random() * 4;
       if (priceIncrease > 0 && !Number.isNaN(Number(sellAmount))) {
-        if (Number(sellAmount) < 8) {
+        if (Number(sellAmount) < 100) {
           amount = Math.max(Number(sellAmount), amount);
         }
         else {
-          amount = 8;
+          amount = 100;
         }
       }
       let nextBuyPrice = Math.abs(Number(sellPrice ?? buyPrice) + priceIncrease);
