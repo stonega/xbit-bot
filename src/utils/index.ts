9f08a270c5a080e7920a7c7447066a3be8846e2d
stone <xijieyin@gmail.com>
2025-04-10 16:45:26 +0800

    minor fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -159,6 +159,9 @@ export async function getTargetPrice(pair: string, latestPrice: number): Promise
       targetPrice = currentApiPrice / 80000;
     }
     targetPrice = (Math.ceil(targetPrice * 100000)) / 100000;
+    if (targetPrice < 0.001 || targetPrice > 0.002) {
+      targetPrice = 0.0015;
+    }
     db.run("UPDATE prices SET price = ? WHERE timestamp = ?", [targetPrice, currentMinute]);
     return targetPrice;
   }
