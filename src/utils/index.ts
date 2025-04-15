d25daa198d184810733a43775af67f0c9ff82c78
stone <xijieyin@gmail.com>
2025-04-15 10:59:26 +0800

    fix: minor fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -149,7 +149,7 @@ export async function getTargetPrice(pair: string, latestPrice: number): Promise
       }
       else {
         // Limit max change rate to 0.02
-        const scaledChangeRate = change * 20;
+        const scaledChangeRate = change * 30;
         const changeRate = scaledChangeRate < -0.1 ? -0.1 : scaledChangeRate > 0.1 ? 0.1 : scaledChangeRate;
         targetPrice = previousPrice * (1 + changeRate);
         console.log(change * 30, { previousApiPrice, currentApiPrice, previousPrice, targetPrice });
