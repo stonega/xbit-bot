cb207c7e23e6a52c8676cdc013c2f3b13e1f4e88
stone <xijieyin@gmail.com>
2025-12-11 15:27:12 +0800

    feat: enable SOL-USDT trading pair configuration

--- src/config.ts
+++ src/config.ts
@@ -10,17 +10,17 @@ export const PAIRS = [
     decimals: 18,
     minSizeDecimals: 3,
   },
-  // {
-  //   symbol: "SOL-USDT",
-  //   price: "SOL",
-  //   makerPrivateKey: Bun.env.MAKER_PRIVATE_KEY_SOL,
-  //   takerPrivateKey: Bun.env.TAKER_PRIVATE_KEY_SOL,
-  //   marketId: 3,
-  //   takerAccount: Bun.env.TAKER_ACCOUNT_SOL,
-  //   makerAccount: Bun.env.MAKER_ACCOUNT_SOL,
-  //   decimals: 9,
-  //   minSizeDecimals: 2,
-  // },
+  {
+    symbol: "SOL-USDT",
+    price: "SOL",
+    makerPrivateKey: Bun.env.MAKER_PRIVATE_KEY_SOL,
+    takerPrivateKey: Bun.env.TAKER_PRIVATE_KEY_SOL,
+    marketId: 3,
+    takerAccount: Bun.env.TAKER_ACCOUNT_SOL,
+    makerAccount: Bun.env.MAKER_ACCOUNT_SOL,
+    decimals: 9,
+    minSizeDecimals: 2,
+  },
 ];
 
 // Max order amount for taker
