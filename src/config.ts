b29e14f2b57ad4a52a059fa4b6c9ee9b49dc54d6
stone <xijieyin@gmail.com>
2026-03-13 14:41:05 +0800

    chore: Update `pairId` values in the configuration.

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const PAIRS = [
     makerPrivateKey: Bun.env.MAKER_PRIVATE_KEY,
     takerPrivateKey: Bun.env.TAKER_PRIVATE_KEY,
     marketId: 3,
-    pairId: "0x950c1bb15508369148679bf2921417929f1465c068c4b22a980c3c23535846c0",
+    pairId: "0xc3f5b2f3656333509760d98f8f7d3022b6ee28eb9c37e805f6c2db245103ba1b",
     takerAccount: Bun.env.TAKER_ACCOUNT,
     makerAccount: Bun.env.MAKER_ACCOUNT,
     decimals: 18,
@@ -19,7 +19,7 @@ export const PAIRS = [
     takerAccount: Bun.env.TAKER_ACCOUNT_SOL,
     makerAccount: Bun.env.MAKER_ACCOUNT_SOL,
     decimals: 9,
-    pairId: '0x7219b7b8ceab0580a3124c284f9bda81dfef3309f28e50c08df32711967a489d',
+    pairId: '0x8a4ddc173cfc745cb7617fa4be54172d2e3bdc2aa5aaa61b92123c0ad998604f',
   },
 ];
 
